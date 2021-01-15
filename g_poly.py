f33359d235490eeb6e4abc16e03e46a692ca4952
Yiyan Shou <74117931+yiyanshou@users.noreply.github.com>
2021-01-15 18:12:21 -0500

    Add files via upload

--- g_poly.py
+++ g_poly.py
@@ -1,5 +1,8 @@
 # g-Polynomial
 from sympy import symbols, expand, poly, init_printing
+from sympy.liealgebras.weyl_group import WeylGroup
+from sympy.liealgebras.root_system import RootSystem
+from sympy.matrices import Matrix, zeros, eye, diag
 from math import floor
 from IPython.display import display
 from itertools import permutations, combinations
@@ -13,7 +16,10 @@ t = symbols('t')
 class Complex:
     def __init__(self, max_cells):
         self.max_cells = max_cells
-        self.dim = max([cell.dim for cell in max_cells])
+        if len(max_cells) == 0:
+            self.dim = -1
+        else:
+            self.dim = max([cell.dim for cell in max_cells])
         self.f = tuple()
         self.g = -1
         self.h = -1
@@ -26,7 +32,7 @@ class Complex:
     
     #Set of cells of dimension dim
     def get_cells(self, dim):
-        return self.cells[dim]
+        return set(self.cells[dim])
     
     #Set of all cells
     def get_all_cells(self):
@@ -37,7 +43,7 @@ class Complex:
     
     #Set of maximal cells
     def get_max_cells(self):
-        return self.max_cells
+        return set(self.max_cells)
     
     #List of the names of all cells organized according to dimension
     def display_cells(self):
@@ -65,6 +71,16 @@ class Complex:
         self.max_cells -= red_cells
         return self
     
+    #Deletes a cell and all cells containing that cell from the complex
+    def delete(self, cell):
+        new_cells = set()
+        for c in self.get_all_cells():
+            if not (cell in c.get_all_faces() or c in cell.get_all_faces()):
+                new_cells.add(c)
+        self.__init__(new_cells)
+        self.clean()
+        return(self)
+    
     #Returns the f-vector of the complex as a tuple of ints   
     def f_vec(self):
         if self.f == tuple():
@@ -74,6 +90,10 @@ class Complex:
     #Returns the h-polynomial as a sympy expression in t
     def h_poly(self):
         if self.h == -1:
+            if self.dim == -1:
+                self.h = 1
+                return self.h
+            
             self.h = (t - 1)**self.dim
             facet_comp = Complex(self.get_cells(self.dim - 1))
             if facet_comp.is_simplicial():
@@ -114,7 +134,7 @@ class Cell:
 
     #Set of faces of dimension dim                    
     def get_faces(self, dim):
-        return self.faces[dim]
+        return set(self.faces[dim])
     
     #Set of all faces
     def get_all_faces(self):
@@ -127,7 +147,7 @@ class Cell:
     def get_facets(self):
         if self.dim == 0:
             return set()
-        return self.faces[self.dim - 1]
+        return set(self.faces[self.dim - 1])
     
     #Returns true if polytope is a simplex
     def is_simplex(self):
@@ -178,7 +198,7 @@ def bary_sub(comp, level = 0, B = {}, C = {}):
         return Complex(new_cells)
 
 #Barycentric subdivision of the boundary of a complex (up to the level-
-#skeleton. When the complex consists of a single polytope P, refine(P) is the 
+#skeleton). When the complex consists of a single polytope P, refine(P) is the 
 #polytope corresponding to the resolution of the toric variety associated to P.
 def refine(comp, level = 0):
     facet_comp = Complex(comp.get_cells(comp.dim - 1))
@@ -247,6 +267,13 @@ def cube(n):
     if n == 1:
         return L
     return product(cube(n - 1), L)
+
+#n-simplex as a simplicial complex
+def simplex(n):
+    if n == 0:
+        return Complex({vertex()})
+    
+    return cone(simplex(n-1))
 
 
 # type A permutohedron
@@ -322,6 +349,239 @@ def permutohedron(n):
                 
     return Complex({cell[(frozenset(range(1, n + 2)),)]})
 
+# Resolutions, orbits, and fibres
+    
+#Returns st(cell), the polytope corresponding to the closure of the orbit
+#corresponding to cell.
+def star(comp, cell):
+    if not cell in comp.get_all_cells():
+        return None
+    
+    if cell in comp.get_max_cells():
+        return Complex(set())
+    
+    pre_st = [set() for i in range(cell.dim, comp.dim + 1)]
+    for c in comp.get_all_cells():
+        if cell in c.get_all_faces():
+            pre_st[c.dim - cell.dim].add(c)
+            
+    st_quot = {}
+    for c in pre_st[1]:
+        st_quot[c] = vertex()
+        
+    for i in range(2, len(pre_st)):
+        for c in pre_st[i]:
+            pre_facets = c.get_facets().intersection(pre_st[i - 1])
+            facets = set([st_quot[pf] for pf in pre_facets])
+            st_quot[c] = Cell(i - 1, facets)
+            
+    st_max = st_quot[list(comp.get_max_cells())[0]]
+    return Complex({st_max})
+
+#Performs stellar subdivision of cell within comp. Returns the corresponding
+#resolution, center, generic fibre.
+    
+#def stellar_sub(comp, cell):
+#    if cell not in comp.get_all_cells():
+#        return None
+#    if cell.dim == 0:
+#        return (Complex(comp.get_max_cells()), star(comp, cell),
+#                Complex({cell}))
+#    
+#    r = vertex(name = 'r')
+#    cell_sub = cone(Complex(cell.get_facets()), v = r)
+#    new_cells = {}
+#    
+#    new_cells[r] = r
+#    for v in comp.get_cells(0):
+#        new_cells[v] = v
+#        
+#    for i in range(1, comp.dim + 1):
+#        if i <= cell_sub.dim:
+#            cells = comp.get_cells(i) | cell_sub.get_cells(i)
+#        else:
+#            cells = comp.get_cells(i)
+#        for c in cells:
+#            facets = c.get_facets()
+#            if cell in facets:
+#                facets.remove(cell)
+#                facets |= cell_sub.get_max_cells()
+#            
+#            new_facets = set(map(new_cells.get, facets))
+#            new_cells[c] = Cell(c.dim, new_facets)
+#            
+#    new_max_cells = set()
+#    for max_cell in comp.get_max_cells():
+#        if cell == max_cell:
+#            new_max_cells |= set(map(new_cells.get, cell_sub.get_max_cells()))
+#        else:
+#            new_max_cells.add(new_cells[max_cell])
+#    return (Complex(new_max_cells), star(comp, cell), Complex({cell}))
+    
+def stellar_sub(comp, cell):
+    if cell not in comp.get_all_cells():
+        return None
+    if cell.dim == 0:
+        return (Complex(comp.get_max_cells()), star(comp, cell),
+                Complex({cell}))
+    
+    r = vertex(name = 'r')
+    C = Complex(comp.get_max_cells())
+    C.delete(cell)
+    sub = cone(C, r)
+    max_cells = list(sub.get_max_cells())
+    for i in range(len(max_cells)):
+        for j in range(i + 1, len(max_cells)):
+            facets1 = max_cells[i].get_facets()
+            facets2 = max_cells[j].get_facets()
+            for c in facets1.intersection(facets2):
+                sub.delete(c)
+    
+    new_max_cell = Cell(comp.dim, sub.get_max_cells())
+
+    return (Complex({new_max_cell}), star(comp, cell), Complex({cell}))
+
+
+#Weyl Polytopes
+class Weyl_Polytope:
+    #Lie type specified as string, e.g. lie = "A2", and weight as column 
+    #vector w.r.t. basis of simple roots, e.g. weight = Matrix([[1], [1]]))
+    def __init__(self, lie, weight):
+        self.lie = lie
+        self.R = RootSystem(lie)
+        self.W = WeylGroup(lie)
+        self.weight = weight
+        self.rank = len(self.R.simple_roots())
+        self.comp = None
+        self.sr = self.R.simple_roots()
+        self.fw = None
+        
+        a_raw = self.R.simple_roots()
+        self.amb_dim = len(a_raw[1])
+        self.a = {}
+        for i in a_raw.keys():
+            self.a[i] = Matrix([a_raw[i]]).transpose()
+            
+        self.C = zeros(self.amb_dim, len(self.a))
+        for j in range(len(self.a)):
+            self.C[:, j] = self.a[j + 1]
+            
+    #Returns a dictionary of simple roots
+    def simple_roots(self):
+        r = {}
+        for i in self.sr.keys():
+            r[i] = Matrix([self.sr[i]]).T
+        return r
+    
+    #Returns a dictionary of fundamental weights
+    def fundamental_weights(self):
+        if not self.fw == None:
+            return self.fw.copy()
+        
+        coroots = self.C.T
+        for i in range(self.rank):
+            coroots[i, :] *= 2 / (Matrix(self.sr[i + 1]).norm() ** 2)
+        coroots *= self.C
+        
+        fund = {}
+        self.Cw = zeros(self.amb_dim, self.rank)
+        for j in range(self.rank):
+            fund[j + 1] = self.C * coroots.LUsolve(eye(self.rank)[:, j])
+            self.Cw[:, j] = fund[j + 1]
+            
+        self.fw = fund  
+        return fund
+            
+    #Reexpresses a column vector in the basis of simple roots    
+    def expand_in_roots(self, v):
+        return self.C.LUsolve(v)
+    
+    #Reexpresses a column vector in the basis of fundamental weights
+    def expand_in_weights(self, v):
+        if self.fw == None:
+            self.fundamental_weights()
+            
+        return self.Cw.LUsolve(v)
+    
+    #Applies Weyl group element w to column vector v in basis of simple roots
+    def act(self, w, v):
+        return self.expand_in_roots(self.W.matrix_form(w) * self.C * v)
+    
+    #Generates a parabolic Weyl group from a list of simple reflections
+    #e.g. ["r1", "r2", "r4"]
+    def para_weyl(self, generators):        
+        w_str = [""]
+        w_mat = [eye(self.amb_dim)]
+        para = {"" : eye(self.amb_dim)}
+            
+        def gen(prefix, prev):             
+            for r in [w for w in generators if not w == prev]:
+                if len(prefix) > 0:
+                    cur_str = prefix + '*' + r
+                else:
+                    cur_str = r
+                cur_mat = self.W.matrix_form(cur_str)
+                if not cur_mat in w_mat:
+                    w_str.append(cur_str)
+                    w_mat.append(cur_mat)
+                    para[cur_str] = cur_mat
+                    gen(cur_str, r)
+            return para 
+        
+        return gen("", self.W.generators())
+    
+    #Builds the Weyl polytope as a Complex    
+    def get_complex(self):
+        weyl = self.para_weyl(self.W.generators())
+        skeleta = [set() for i in range(self.rank + 1)]
+        para = {}
+        for i in range(self.rank):
+            for para_gens in combinations(self.W.generators(), i):
+                para = self.para_weyl(para_gens)
+                IC_face = []
+                for w_mat in para.values():
+                    IC_face.append(self.expand_in_roots(w_mat * self.C 
+                                                         * self.weight))
+                for w in weyl:
+                    face = frozenset(map(lambda v : tuple(self.act(w, v)),
+                                         IC_face))
+                    skeleta[i].add(face)
+        
+        max_face = []            
+        for vert in skeleta[0]:
+            max_face.append(list(vert)[0])
+        skeleta[self.rank].add(frozenset(max_face))
+        
+        return complex_from_hulls(skeleta)
+    
+    #Symmetrizes v, a column vector in the basis of simple roots, w.r.t. 
+    #a list, refls, of simple reflections
+    def sym(self, refls, v):
+        para = self.para_weyl(refls)
+        s = zeros(self.rank, 1)
+        for w_mat in para.values():
+            s += self.expand_in_roots(w_mat * self.C * v)
+            
+        return s / len(para)
+    
+    def simplex(self, perm):
+        verts = []
+        for i in range(len(perm) + 1):
+            verts.append(self.sym(perm[:i], self.weight))
+        return verts
+        
+    
+P = Weyl_Polytope("A3", Matrix([[3], [4], [3]]))
+span = eye(P.rank)
+verts = P.simplex(["r3", "r2"])
+for j in range(len(verts)):
+    span[:, j] = P.expand_in_weights(P.C * verts[j])
+display(span)
+print("Det = {}".format(span.det()))
+
+
+
+
 #---------------------code ends here----------------------------
 
 
@@ -369,12 +629,66 @@ A2 = complex_from_hulls([[{1}, {2}, {3}, {4}, {5}, {6}],
 # square with diagonal
 S = complex_from_hulls([[{1}, {2}, {3}, {4}], [{1,2}, {2,3}, {3,4}, {1,4},
                         {1,3}], [{1, 2, 3}, {1, 4, 3}]])
+    
+#test
+S = cube(4)
+c = S.get_cells(2).pop()
+Test = stellar_sub(S, c)[0]
 
+    
+# resolution conjecture tests
+C = permutohedron(4)             #polytope
+d = 2             #dimension of faces to subdivide
+verbose = True    #prints various stages of the calculation
+#
+#print("***h-poly C***")
+#print(expand(C.h_poly()))
+#print("\n")
+#
+#i = 1
+#true = 0
+#false = 0
+#    
+#for c in C.get_cells(d):
+#    sub = stellar_sub(C, c)
+#    
+#    print("subdivision complete \n")
+#    
+#    if verbose:
+#        print("***h-poly C\' (face " + str(i) + ")***")
+#        print(expand(sub[0].h_poly()))
+#        print("\n")
+#        
+#        print("h-poly center")
+#        print(expand(sub[1].h_poly()))
+#        print("\n")
+#        
+#        print("h-poly fibre")
+#        print(expand(sub[2].h_poly()))
+#        print("\n")
+#        
+#        print("h-poly difference")
+#        print(expand(sub[0].h_poly() - C.h_poly()))
+#        print("\n")
+#        
+#        print("conjectured difference")
+#        print(expand(sub[1].h_poly() * (sub[2].h_poly() 
+#            - sub[2].g_poly())))
+#        print("\n")
+#
+#    if expand(sub[0].h_poly() - C.h_poly()) == expand(sub[1].h_poly() 
+#            * (sub[2].h_poly() - sub[2].g_poly())):
+#        true += 1
+#    else:
+#        false += 1
+#        
+#    print("true: " + str(true) + ", false: " + str(false) + "\n")
+#    i += 1
     
     
 # h-polynomial tests
-n = 4
-C = permutohedron(n)
+#n = 4
+#C = permutohedron(n)
 
 #step-by-step subdivision of A_n permutohedron
 
@@ -400,8 +714,8 @@ C = permutohedron(n)
 #    display(expand(l.h_poly()))
 #    print("------------------------------")
 
-R2C = refine(C, 2)
-print(expand(R2C.h_poly())
+#R2C = refine(C, 2)
+#print(expand(R2C.h_poly())
 
 
     
